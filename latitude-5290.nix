dcd031754083a7b0a4d9bb4204aaa388ca0a73db
bvgcat <bvgcat@gmail.com>
2025-01-07 12:54:46 +0100

    updated latitude-5290

--- latitude-5290.nix
+++ latitude-5290.nix
@@ -4,26 +4,72 @@ let
 	pkgs-24 = import <nixos-24.05> {};
 	pkgs-unstable = import <nixos-unstable> {};
 in {
-
-	
   imports = [
 		./modules/common.nix
 		./nixos-hardware/dell/latitude/7280/default.nix # surface go hardware
 	];
 
-	boot.kernelModules = [ "snd_hda_intel" ];
-  boot.supportedFilesystems = [ "ntfs" ];
-	hardware.bluetooth.enable = true;
+	### Hardware
 
+	boot = {
+		## to detect windows
+		kernelParams = [ "mem_sleep_default=deep" ];
+		kernelPackages = pkgs.linuxPackages_latest;
+		kernelModules = [ "snd_hda_intel" ];
+		supportedFilesystems = [ "ntfs" ];
+	};
+		
+	hardware.bluetooth.enable = true;
+  hardware.enableAllFirmware = true;
+	hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+			intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
+    ];
+  };
+	environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
+
+	services = {
+    thermald.enable = true;
+    power-profiles-daemon = {
+			enable = true;
+			package = pkgs.power-profiles-daemon;
+		};
+		auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
+	};
+
+	### Packages
   environment.systemPackages = with pkgs; [];
 
 	users.users.h.packages = with pkgs; [
 		pkgs-unstable.freecad
 		kicad
 		pkgs-unstable.octaveFull
-		signal-desktop
 	];
 
-    swapDevices = [ { device = "/swapfile"; size = 8128; } ];
+	#systemd.services.fprintd = {
+	#	wantedBy = [ "multi-user.target" ];
+	#	serviceConfig.Type = "simple";
+	#};
+
+	#services.fprintd = {
+	#	enable = true;
+ 	#	tod.enable = true;
+	#	tod.driver = pkgs-unstable.libfprint-2-tod1-broadcom;
+	#};
+  swapDevices = [ { device = "/swapfile"; size = 8128; } ];
 }
 
