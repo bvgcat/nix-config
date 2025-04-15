54c304088e7c9f483e937a8d57ed5ddb6bf1d9af
bvgcat <bvgcat@gmail.com>
2025-04-15 04:02:59 +0200

    added firewall exceptions removed comments

--- surface-go/part-db.nix
+++ surface-go/part-db.nix
@@ -1,29 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
-  #systemd.services.part-db = {
-  #  description = "PartDB PHP Server";
-  #  wantedBy = [ "multi-user.target" ];
-  #  after = [ "network.target" ];
-
-  #  serviceConfig = {
-  #    User = "part-db";
-  #    Group = "part-db";
-  #    WorkingDirectory = "${pkgs.part-db}";  # Adjust if needed
-  #    ExecStart = "${pkgs.php}/bin/php -d memory_limit=256M ${pkgs.part-db}/bin/console server:run localhost:5432";
-  #    Restart = "always";
-  #    ProtectSystem = "full";
-  #    ProtectHome = true;
-  #    PrivateTmp = true;
-  #    NoNewPrivileges = true;
-  #  };
-  #};
-
   services.part-db = {
     enable = true;
     package = pkgs.part-db;
     enablePostgresql = true;
-    # done by enablePostgresql
 
     settings = {
       # DATABASE_URL="postgresql://db_user@localhost/db_name?serverVersion=16.6&charset=utf8&host=/var/run/postgresql"
@@ -42,4 +23,8 @@
       host    part-db   part-db   ::1/128         scram-sha-256
     '';
   };
+
+  # allow the ports used for php through
+	networking.firewall.allowedTCPPorts = [ 80 ];
+	networking.firewall.allowedUDPPorts = [ 80 ];
 }
