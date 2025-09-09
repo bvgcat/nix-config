7283851a0ef9ecaccb2628368d0355d3c6566f96
none <none@none.com>
2025-09-09 23:05:30 +0200

    declerative nixd integration

--- modules/codium.nix
+++ modules/codium.nix
@@ -1,37 +1,24 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, lib, ... }:
 
-with lib;
-
-let
-  cfg = config.programs.nixd-codium;
-in
 {
-  options.programs.nixd-codium = {
-    enable = mkEnableOption "Nixd Codium temporary environment";
-  };
-
-  config = mkIf cfg.enable {
-    environment.systemPackages = [
-      (pkgs.writeShellScriptBin "codium-test" ''
-        set -e
-        dir="''${XDG_CACHE_HOME:-~/.cache}/nixd-codium"
-        ${pkgs.coreutils}/bin/mkdir -p "$dir/User"
-        cat >"$dir/User/settings.json" <<EOF
-        {
-        "security.workspace.trust.enabled": false,
-        "nix.enableLanguageServer": true,
-        "nix.serverPath": "nixd",
-        }
-        EOF
-        ${pkgs.vscode-with-extensions.override {
-          vscode = pkgs.vscodium;
-          vscodeExtensions = with pkgs.vscode-extensions; [
-            jnoortheen.nix-ide
-          ];
-        }}/bin/codium --user-data-dir "$dir" "$@"
-      '')
-    ];
-  };
-  programs.nixd.enable = true;
-  programs.nixd-codium.enable = true;
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "codium-test" ''
+      set -e
+      dir="''${XDG_CACHE_HOME:-~/.cache}/nixd-codium"
+      ${pkgs.coreutils}/bin/mkdir -p "$dir/User"
+      cat >"$dir/User/settings.json" <<EOF
+      {
+      "security.workspace.trust.enabled": false,
+      "nix.enableLanguageServer": true,
+      "nix.serverPath": "nixd",
+      }
+      EOF
+      ${pkgs.vscode-with-extensions.override {
+        vscode = pkgs.vscodium;
+        vscodeExtensions = with pkgs.vscode-extensions; [
+          jnoortheen.nix-ide
+        ];
+      }}/bin/codium --user-data-dir "$dir" "$@"
+    '')
+  ];
 }
