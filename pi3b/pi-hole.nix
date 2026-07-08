82226ce8684d419051f8430605c3bcf33989fb57
John Doe <johndoe@example.com>
2026-07-08 21:57:05 +0200

    fixed addresses

--- pi3b/pi-hole.nix
+++ pi3b/pi-hole.nix
@@ -16,32 +16,30 @@
     openFirewallDHCP = true;
 
     settings = {
-      # Upstream DNS resolvers
       dns = {
         upstreams = [ "1.1.1.1" "9.9.9.9" ];
+
         hosts = [
+          # LAN services
           "192.168.0.100 pi.hole"
           "192.168.0.100 pi.hole.pi"
-          "192.168.0.110 immich.homeserver"
-          "192.168.0.110 home.homeserver"
-          "192.168.0.110 cloud.homeserver"
-          "192.168.0.110 sync.homeserver"
-          "192.168.0.110 assistant.homeserver"
-          "192.168.0.110 lounge.homeserver"
+          "192.168.0.112 immich.homeserver"
+          "192.168.0.112 home.homeserver"
+          "192.168.0.112 cloud.homeserver"
+          "192.168.0.112 sync.homeserver"
+          "192.168.0.112 assistant.homeserver"
+          "192.168.0.112 lounge.homeserver"
+
+          # WG
+          "10.100.0.1 pi.hole"
+          "10.100.0.1 pi.hole.pi"
           "10.100.0.1 pi3b"
           "10.100.0.2 homeserver"
           "10.100.0.3 thinkpad-l14-g2"
           "10.100.0.4 pixel-7"
           "10.100.0.5 surface-go"
-          "10.100.0.1 pi.hole"
-          "10.100.0.1 pi.hole.pi"
-          "10.100.0.2 immich.homeserver"
-          "10.100.0.2 home.homeserver"
-          "10.100.0.2 cloud.homeserver"
-          "10.100.0.2 sync.homeserver"
-          "10.100.0.2 assistant.homeserver"
-          "10.100.0.2 lounge.homeserver"
         ];
+
         listenInterface = "all";
       };
     };
