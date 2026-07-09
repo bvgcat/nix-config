26a0f6e490e8011b51a52c978e2d6fa9b4cf1cbc
John Doe <johndoe@example.com>
2026-07-09 15:35:23 +0200

    removed wg ips

--- pi3b/pi-hole.nix
+++ pi3b/pi-hole.nix
@@ -19,11 +19,9 @@
       dns = {
         upstreams = [ "1.1.1.1" "9.9.9.9" ];
         hosts = [
+          "192.168.0.100 pi3b"
           "192.168.0.100 pi.hole"
           "192.168.0.100 pi.hole.pi"
-          "10.100.0.1 pi.hole"
-          "10.100.0.1 pi.hole.pi"
-          "10.100.0.1 pi3b"
 
           "192.168.0.112 homeserver"
           "192.168.0.112 immich.homeserver"
@@ -36,18 +34,6 @@
           "192.168.0.113 thinkpad-l14-g2"
           "192.168.0.114 pixel-7"
           "192.168.0.115 surface-go"
-
-          "10.100.0.2 homeserver"
-          "10.100.0.2 immich.homeserver"
-          "10.100.0.2 home.homeserver"
-          "10.100.0.2 cloud.homeserver"
-          "10.100.0.2 sync.homeserver"
-          "10.100.0.2 assistant.homeserver"
-          "10.100.0.2 lounge.homeserver"
-
-          "10.100.0.3 thinkpad-l14-g2"
-          "10.100.0.4 pixel-7"
-          "10.100.0.5 surface-go"
         ];
         listenInterface = "all";
       };
